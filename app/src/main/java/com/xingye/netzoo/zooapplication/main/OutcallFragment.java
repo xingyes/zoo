557165850140c68e1e353c7ed71990bc01c20eb0
yaoxing6 <yaoxing1@jd.com>
2017-06-20 16:31:51 +0800

    专家出诊 fragment 框架ok 待确认 一级分类／二级分类

--- app/src/main/java/com/xingye/netzoo/zooapplication/main/OutcallFragment.java
+++ app/src/main/java/com/xingye/netzoo/zooapplication/main/OutcallFragment.java
@@ -1,20 +1,53 @@
 package com.xingye.netzoo.zooapplication.main;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.xingye.netzoo.zooapplication.R;
 
 
 public class OutcallFragment extends BaseFragment{
 
+    private RecyclerView cateListv;
+    private ImageView    delv;
+    private EditText     searchEdit;
+
     @Override
     public View initView() {
         mRoot = mContext.getLayoutInflater().inflate(R.layout.fragment_outcall,null);
+        delv = (ImageView)mRoot.findViewById(R.id.search_del);
+        searchEdit = (EditText)mRoot.findViewById(R.id.search_keyword);
+        searchEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.length()>0)
+                    delv.setVisibility(View.VISIBLE);
+                else
+                    delv.setVisibility(View.INVISIBLE);
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+        delv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchEdit.setText("");
+            }
+        });
+
+        cateListv = (RecyclerView)mRoot.findViewById(R.id.outcall_list);
         return mRoot;
     }
 
